865e0b87638d4dfe0be7325967a4de7a45406466
daveless <44807207+Daveless@users.noreply.github.com>
2025-02-06 09:50:50 -0500

    fix phone number

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -5,7 +5,7 @@ export const SITE_CONFIG = {
   contact: {
     phone: '+123456789',
     email: 'contacto@wrs-arquitectura.com',
-    whatsapp: '+123456789',
+    whatsapp: '+593993272165',
     address: 'Tu dirección aquí',
     schedule: 'Lun - Vie: 9:00 - 18:00'
   }
